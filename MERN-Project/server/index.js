b156f96d72e96d66e9a7be9a76b75df6f47b2120
Ktnnn3 <59277393+Ktnnn3@users.noreply.github.com>
2022-01-10 15:29:16 +0700

    Update index.js

--- MERN-Project/server/index.js
+++ MERN-Project/server/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://gudeicnx:guide31320@cluster0.2chzk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const CONNECTION_URL = 'mongodb+srv://se:se1234@cluster0.nre6b.mongodb.net/seDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
